0abbce16a2532df1ef387bd9f9ca48836faa0f43
alecillo <sergiovr2304@gmail.com>
2024-02-09 13:48:02 -0600

    this is my first commit

---
+++ src/main/kotlin/com/codely/demo/Codelyber.kt
@@ -0,0 +1,10 @@
+package com.codely.demo
+
+class Codelyber {
+    fun greet() = "SALCHIPAPA CON ALITAS BUFALO"
+}
+
+fun main() {
+    println(Codelyber().greet())
+    println("Sergio Alejandro Villa Reyna")
+}
